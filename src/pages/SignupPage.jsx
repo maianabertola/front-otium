5c4c9a0bd10b00655037d11fe751dda735e94057
Taupin Fabien <taupinfabien2607@gmail.com>
2023-06-08 16:10:51 +0200

    fixing imports

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 import AuthForm from "../components/AuthForm";
-import "../pages/signupPage.css";
+import "../pages/SignupPage.css";
 
 const collectionUser = "http://localhost:3000/auth/signup";
 function SignupPage() {
