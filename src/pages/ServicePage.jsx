1fe0ebadfa41a31ae8fe81bb43b68d790458d216
maianabertola <maia@we-are-ensemble.com>
2023-07-04 18:05:45 +0200

    servicePage done

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -4,9 +4,13 @@ import { useParams } from "react-router-dom";
 import service from "../service/service";
 import "./ServicePage.css";
 import imgService from "../assets/ChefService.jpg";
+import TitlePage from "../components/TitlePage";
+import { Link } from "react-router-dom";
+import VillaCard from "../components/VillaCard";
 
 function ServicePage() {
   const [oneService, setOneService] = useState(null);
+  const [villas, setVillas] = useState(null);
   const { id } = useParams();
 
   //fetching the services
@@ -19,28 +23,66 @@ function ServicePage() {
     }
   };
 
+  //fetch data from the villa
+  const getAllVillas = async () => {
+    try {
+      const myVillas = await service.get("/villa");
+      setVillas(myVillas.data);
+    } catch (error) {
+      console.log(
+        "there is an error when fetching all the villas from db on the homepage"
+      );
+    }
+  };
+
   //use effect to fire these functions once the page is loaded
   useEffect(() => {
-    getOneService();
+    getOneService(), getAllVillas();
   }, []);
 
-  if (!oneService) {
+  if (!oneService || !villas) {
     return <div>Please wait a moment</div>;
   }
-  console.log(oneService);
+
+  //filter the villas if they include the service displayed
+  let villasToDisplay = villas.Villa.filter((villa) => {
+    // console.log(villa.services);
+    return villa.services.includes(oneService.Service._id);
+    // console.log(oneService.Service._id);
+  });
+
   return (
-    <div className="pageContainer flexrowService">
-      <div className="imgServiceContainer">
-        <img src={imgService}></img>
-      </div>
-      <div className="greenBackgroundService">
-        <div className="textServiceContainer">
-          <h1>{oneService.Service.title}</h1>
-          <h2>{oneService.Service.slogan}</h2>
-          <p>{oneService.Service.description}</p>
+    <>
+      <div className="pageContainer flexrowService">
+        <div className="imgServiceContainer">
+          <img src={imgService}></img>
+        </div>
+        <div className="greenBackgroundService">
+          <div className="textServiceContainer">
+            <h1>{oneService.Service.title}</h1>
+            <h2>{oneService.Service.slogan}</h2>
+            <p>{oneService.Service.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+      <TitlePage h1={"This service is"} span={"available in"}></TitlePage>
+      <div className="collectionContainer">
+        {villasToDisplay.map((villa) => {
+          return (
+            <>
+              <Link to={`/villa/${villa._id}`}>
+                <VillaCard
+                  key={villa._id}
+                  region={villa.region}
+                  name={villa.name}
+                  slogan={villa.slogan}
+                ></VillaCard>
+              </Link>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
