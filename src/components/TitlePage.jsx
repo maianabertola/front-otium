a64800cdafa4e79e3ab23708e1010c35d7cd423b
maianabertola <maia@we-are-ensemble.com>
2023-07-07 16:32:47 +0200

    confirmationBookingPage done

--- src/components/TitlePage.jsx
+++ src/components/TitlePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./TitlePage.css";
 
-function TitlePage({ h1, span }) {
+function TitlePage({ h1, span, center }) {
+  let myStyle;
+  if (center === "true") {
+    myStyle = {
+      textAlign: "center",
+    };
+  }
   return (
     <div className="titlePageContainer">
-      <h1>
+      <h1 style={myStyle}>
         {h1} <span> {span}</span>
       </h1>
     </div>
--- src/pages/ConfirmationBookingPage.jsx
+++ src/pages/ConfirmationBookingPage.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import TitlePage from "../components/TitlePage";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 function ConfirmationBookingPage() {
+  const navigate = useNavigate();
+
+  function navtoAccount(event) {
+    console.log("toto");
+    event.preventDefault();
+    navigate("account");
+  }
   return (
-    <div>
-      <h1>Your booking has been successfully created</h1>
-      <p>
+    <div className="pageContainer">
+      <TitlePage
+        h1={"Your booking has been"}
+        span={"successfully created"}
+        center={"true"}
+      ></TitlePage>
+      <p style={{ textAlign: "center" }}>
         Thank you for your trust. Your request has been successfully recorded.
+        <br />
         You can find{" "}
         <Link to="/account-trips"> all your reservations in your account.</Link>
       </p>
+      <div className="flexCenter">
+        <Button
+          cta={"Find all my reservations"}
+          backgroundColor={"black"}
+          url={navtoAccount}
+        ></Button>
+      </div>
     </div>
   );
 }
