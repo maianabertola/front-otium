bfb9569fef45315ff5096ed121a28028e38a4196
maianabertola <maia@we-are-ensemble.com>
2023-08-01 15:52:02 +0200

    inserted react query in servicesCollectionPage

--- src/pages/ServicesCollectionPage.jsx
+++ src/pages/ServicesCollectionPage.jsx
@@ -2,34 +2,26 @@ import React from "react";
 import NeedHelp from "../components/NeedHelp";
 import TitlePage from "../components/TitlePage";
 import "../App.css";
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ServiceCard from "../components/ServiceCard";
-import service from "../service/service";
 import serviceImg from "../assets/ChefService.jpg";
+import { useQuery } from "react-query";
+import { getAllServices } from "../api/services";
 
 function ServicesCollectionPage() {
-  const [services, setServices] = useState(null);
+  const {
+    isLoading,
+    isError,
+    error,
+    data: services,
+  } = useQuery({ querykey: ["services"], queryFn: getAllServices });
 
-  //fetching the services
-  const getAllServices = async () => {
-    try {
-      const myServices = await service.get("/service");
-      setServices(myServices.data);
-    } catch (error) {
-      console.log(
-        "there is an error when fetching all the services on ServicesCollectionPage"
-      );
-    }
-  };
-
-  //use effect to fire these functions once the page is loaded
-  useEffect(() => {
-    getAllServices();
-  }, []);
-
-  if (!services) {
+  if (isLoading) {
     return <div>Please wait a moment</div>;
+  }
+
+  if (isError) {
+    return <div>There is an error {error}</div>;
   }
 
   return (
