31e96863afe01c284730e075c61a61024992944f
maianabertola <maia@we-are-ensemble.com>
2023-08-18 11:36:07 +0200

    protected routes updated

--- src/pages/ProtectedRoutes.jsx
+++ src/pages/ProtectedRoutes.jsx
@@ -8,7 +8,7 @@ function ProtectedRoutes() {
     return <p>Loading...</p>;
   }
   if (!isLoggedIn) {
-    return <Navigate to={"need-login"} />;
+    return <Navigate to={"auth/need-login"} />;
   }
 
   if (isLoggedIn) {
